0e462658245b33f2eb5d8321543a0c4097455907
linwenjun <linwenjun@163.com>
2020-11-18 15:45:16 +0000

    first commit

---
+++ BullsAndCows/SecretGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BullsAndCows
+{
+    public class SecretGenerator
+    {
+        public string GenerateSecret()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
